77edc51993e241d4310e345295ae550664426f07
INPO0\i874d <garlandta@inpo.org>
2025-07-25 19:03:14 -0400

    Project Portfolio Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { AboutMe } from './about-me/about-me';
+import { Portfolio } from './portfolio/portfolio';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: Home},
-    {path: 'AboutMe', component: AboutMe}
+    {path: 'AboutMe', component: AboutMe},
+    {path: 'Projects', component: Portfolio}
 ];
