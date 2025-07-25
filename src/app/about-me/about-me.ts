1bcb021c77bcaabb8ceb359fe7dd06c6305384c7
INPO0\i874d <garlandta@inpo.org>
2025-07-25 16:51:59 -0400

    Sidebar w/ Anchor Links Updated & Profile Card

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -10,7 +10,6 @@ import { TChart } from '../core/shared/t-chart/t-chart';
   styleUrl: './about-me.css'
 })
 export class AboutMe {
-
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const sections = document.querySelectorAll('section[id]');
@@ -28,6 +27,14 @@ export class AboutMe {
       link.classList.remove('active');
       if (link.getAttribute('href') === `#${current}`) {
         link.classList.add('active');
+
+        const parent = link.closest('.sidebar');
+        if (parent) {
+          const linkTop = link.getBoundingClientRect().top;
+          const parentTop = parent.getBoundingClientRect().top;
+          const offset = linkTop - parentTop - parent.clientHeight / 2;
+          parent.scrollBy({ top: offset, behavior: 'smooth' });
+        }
       }
     });
   }
