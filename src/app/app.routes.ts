1e307ec3486cdb3f4b7f5ef791d136369662723b
INPO0\i874d <garlandta@inpo.org>
2025-07-25 21:45:28 -0400

    "Testimonials Component Created"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './home/home';
 import { AboutMe } from './about-me/about-me';
 import { Portfolio } from './portfolio/portfolio';
 import { SkillsTools } from './skills-tools/skills-tools';
+import { Testimonials } from './testimonials/testimonials';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: Home},
     {path: 'AboutMe', component: AboutMe},
     {path: 'Projects', component: Portfolio},
-    {path: 'Skills', component: SkillsTools}
+    {path: 'Skills', component: SkillsTools},
+    {path:'Testimonials', component: Testimonials}
 ];
